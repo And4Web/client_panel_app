8dc25413f7cdf1af2f5365af0c54bdfea4c4397b
And4Web <anands2807@gmail.com>
2022-02-11 18:40:58 -0800

    AppNavbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AppNavbar from "../src/components/layouts/AppNavbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <AppNavbar />
+        <div className="container">
+          <h1>Hello</h1>
+        </div>
+      </Router>
     </div>
   );
 }
