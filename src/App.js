6393ccff4acc6396673a8b267041ece87d22a655
And4Web <anands2807@gmail.com>
2022-02-12 12:40:35 -0800

    redux added to App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppNavbar from "../src/components/layouts/AppNavbar";
 import DashBoard from "../src/components/layouts/DashBoard";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <AppNavbar />
-        <div className="container">
-          <Routes>
-            <Route exact path="/" element={<DashBoard />} />
-            <Route exact path="/dashboard" element={<DashBoard />} />
-          </Routes>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<DashBoard />} />
+              <Route exact path="/dashboard" element={<DashBoard />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 }
 
--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { createStore, combineReducers, compose } from "redux";
-import firebase from "firebase";
-import "firebase/firestore";
+// import firebase from "firebase";
+// import "firebase/firestore";
 import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
 import { reduxFirestore, firestoreReducer } from "redux-firestore";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 //Reducers
 //@todo
@@ -18,14 +22,32 @@ const firebaseConfig = {
 };
 
 //react-redux-firebase config
-const rrfConfig = {
-  userProfile: 'users'
-  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-}
+// const rrfConfig = {
+//   userProfile: "users",
+//   useFirestoreForProfile: true, // Firestore for Profile instead of Realtime DB
+// };
 
 //Init firebase instance
 firebase.initializeApp(firebaseConfig);
 
 //Init firestore
-const firestore = firebase.firestore();
+// const firestore = firebase.firestore();
 
+// Add firebase to reducers
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer, // <- needed if using firestore
+});
+
+// Create store with reducers and initial state
+const initialState = {};
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    reactReduxFirebase(firebase),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
